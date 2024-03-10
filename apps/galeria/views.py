cd170dac069568c546edd24c27b9468f69fe237d
Marcio Coelho Socorro <m.coelho.socorro@avanade.com>
2024-03-10 18:04:21 -0300

    Conexao com S3 e implementando edição e exclusao de fotos

--- apps/galeria/views.py
+++ apps/galeria/views.py
@@ -29,7 +29,7 @@ def buscar(request):
         if nome_a_buscar:
             fotografias = fotografias.filter(nome__icontains=nome_a_buscar)
 
-    return render(request, 'galeria/buscar.html', {"cards": fotografias})
+    return render(request, 'galeria/index.html', {"cards": fotografias})
 
 def nova_imagem(request):
     if not request.user.is_authenticated:
@@ -46,8 +46,26 @@ def nova_imagem(request):
 
     return render(request, 'galeria/nova_imagem.html', { 'form': form })
 
-def editar_imagem(request):
-    pass
+def editar_imagem(request, foto_id):
+    fotografia = Fotografia.objects.get(id=foto_id)
 
-def deletar_imagem(request):
-    pass
+    form = FotografiaForms(instance=fotografia)
+    if request.method == 'POST':
+        form = FotografiaForms(request.POST, request.FILES, instance=fotografia)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Fotografia editada com sucesso!')
+            return redirect('index')
+
+    return render(request, 'galeria/editar_imagem.html', {'form': form, 'foto_id': foto_id})    
+
+def deletar_imagem(request, foto_id):
+    fotografia = Fotografia.objects.get(id=foto_id)
+    fotografia.delete()
+    messages.success(request, 'Fotografia deletada com sucesso!')
+    return redirect('index')
+
+def filtro(request, categoria):
+    fotografias = Fotografia.objects.order_by("data_fotografia").filter(publicada=True, categoria=categoria)
+
+    return render(request, 'galeria/index.html', {"cards": fotografias})
